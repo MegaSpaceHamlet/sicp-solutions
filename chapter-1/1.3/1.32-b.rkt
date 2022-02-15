edda04c12c2b62d2ac0267b5406ef60fe2da7c2c
MegaSpaceHamlet <mendlegr@gmail.com>
2022-02-15 18:10:30 -0500

    Solved 1.32-b

---
+++ chapter-1/1.3/1.32-b.rkt
@@ -0,0 +1,23 @@
+#lang sicp
+(define (identity x) x)
+
+(define (inc n) (+ n 1))
+
+(define (cube x) (* x x x))
+
+(define (add-two n) (+ n 2))
+
+
+(define (accumulate combiner null-value term a b next)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (combiner result (term a)))))
+  (iter a null-value))
+
+(define (sum term a b next)
+  (accumulate + 0 term a b next))
+
+(define (product term a b next)
+  (accumulate * 1 term a b next))
+
