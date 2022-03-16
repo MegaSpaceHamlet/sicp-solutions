650ec9ba92272aedbd29d517a4e2f5e67621b082
MegaSpaceHamlet <mendlegr@gmail.com>
2022-03-16 17:41:39 -0400

    Much closer to solving the problem (1.38 -- Euler's approximation of 'e'

--- chapter-1/1.3/1.38.rkt
+++ chapter-1/1.3/1.38.rkt
@@ -4,10 +4,15 @@
     (if (> i k)
         (/ (n i) (d i))
         (+ (d i) (/ (n i) (do-cont-frac (+ i 1) n d k)))))
-  (do-cont-frac 0 n d k))
+  (do-cont-frac 1 n d k))
 
 (define (eulers x)
   (cont-frac (lambda (i) 1.0)
-             (lambda (i) 
+             (lambda (i)
+               (if (= 0 (remainder i 3))
+                   (* (/ i 3) 2)
+                   1.0))
+             x))
+
 
                
