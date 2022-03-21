7ee8872f609c9fc47c5a2356dd586947d283f99b
MegaSpaceHamlet <mendlegr@gmail.com>
2022-03-21 11:00:58 -0400

    Solved for Euler's; want to re-examine solution for readability (cleanliness/efficiency)

--- chapter-1/1.3/1.38.rkt
+++ chapter-1/1.3/1.38.rkt
@@ -4,14 +4,17 @@
     (if (> i k)
         (/ (n i) (d i))
         (+ (d i) (/ (n i) (do-cont-frac (+ i 1) n d k)))))
-  (do-cont-frac 1 n d k))
+  (do-cont-frac 0 n d k))
 
 (define (eulers x)
   (cont-frac (lambda (i) 1.0)
              (lambda (i)
+               (if (= i 0)
+                   0
+               (let ((i (+ i 1)))
                (if (= 0 (remainder i 3))
                    (* (/ i 3) 2)
-                   1.0))
+                   1.0))))
              x))
 
 
