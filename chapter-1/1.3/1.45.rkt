69cb270d294c969e7f897acf58b8662f2183774b
Mendel Groner <mendlegr@gmail.com>
2022-05-06 15:59:23 -0400

    Closer to finding the pattern of problem 1.45

--- chapter-1/1.3/1.45.rkt
+++ chapter-1/1.3/1.45.rkt
@@ -10,7 +10,35 @@
                 1.0))
 
 ; Need to test the requirements for damping
+; Although there are unresolved edge cases, e.g. (to get the root of 128, 5 average damps are enough)
+; it seems to be what number of a multiple it is of 2.
+; I'm forgetting the mathematical term, but the authors said we can assume any mathematical operation
+; is available as a primitive.
 (define (nth-root x n)
-  (fixed-point ((repeated average-damp 3)
+  (let ((damp (steps-to-two n)))
+    (fixed-point ((repeated average-damp damp)
+                  (lambda (y) (/ x (expt y (- n 1)))))
+                   1.0)))
+
+(define (test x n damp)
+  (let ((x (expt x n)))
+  (fixed-point ((repeated average-damp damp)
                 (lambda (y) (/ x (expt y (- n 1)))))
-                1.0))
+               1.0)))
+
+(define (my-test base power damp)
+  (define (step i)
+    (display i)
+    (display ": ")
+    (display (test base i damp))
+    (newline)
+    (step (+ i 1)))
+  (step power))
+
+(define (steps-to-two x)
+    (define (step x i)
+      (if (= x 2)
+          i
+          (step (/ x 2) (+ i 1))))
+      (step x 1))
+          
