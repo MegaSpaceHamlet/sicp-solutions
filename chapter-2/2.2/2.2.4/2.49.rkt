201324797430edce3a3a60fa45d9c2cb30322e63
MegaSpaceHamlet <mendlegr@gmail.com>
2022-09-30 13:09:50 -0400

    moving along in wave painter

--- chapter-2/2.2/2.2.4/2.49.rkt
+++ chapter-2/2.2/2.2.4/2.49.rkt
@@ -41,13 +41,21 @@
   (segments->painter
    (list
     (make-segment (make-vect 0.2 0)
-                  (make-vect 0.3 0.5)) ; left leg outer
-    (make-segment (make-vect 0.3 0.5)
+                  (make-vect 0.4 0.5)) ; left leg outer
+    (make-segment (make-vect 0.4 0.5)
                   (make-vect 0.2 0.4)) ; left bicep lower
     (make-segment (make-vect 0.2 0.4)
                   (make-vect 0.0 0.5)) ; left forearm lower
-    (make-segment (make-vect 0.25 0)
-                  (make-vect 0.4 0.45)) ; left left inner
+    (make-segment (make-vect 0.3 0)
+                  (make-vect 0.5 0.3)) ; left leg inner
+    (make-segment (make-vect 0.5 0.3)
+                  (make-vect 0.7 0)) ; right leg inner
+    (make-segment (make-vect 0.8 0)
+                  (make-vect 0.6 0.5)) ; right leg outer
+    (make-segment (make-vect 0.6 0.5)
+                  (make-vect 0.7 0.5)) ;right bicep lower
+    (make-segment (make-vect 0.7 0.5)
+                  (make-vect 1.0 0.3)) ; right forearm lower
     )))
 
 (paint wave)
