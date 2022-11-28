a4c4b7dec42a5f48fe41412b4e6a75886a3b34c0
MegaSpaceHamlet <mendlegr@gmail.com>
2022-11-27 22:15:28 -0500

    Began 2.3.4 -- Huffman Encoding Trees

---
+++ chapter-2/2.3/2.3.4/2.68.scm
@@ -0,0 +1,32 @@
+#lang sicp
+(#%require "huffman-encoding-trees.scm")
+
+(define (encode message tree)
+  (if (null? message)
+      '()
+      (append (encode-symbol (car message) tree)
+              (encode (cdr message) tree))))
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((eq? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+(define (encode-symbol symbol tree)
+  (cond ((leaf? tree) '())
+        ((element-of-set? symbol (symbols (left-branch tree))) (cons '0 (encode-symbol symbol (left-branch tree))))
+        ((element-of-set? symbol (symbols (right-branch tree))) (cons '1 (encode-symbol symbol (right-branch tree))))
+        (else "bad symbol -- ENCODE-SYMBOL" symbol)))
+
+
+(define sample-tree
+  (make-code-tree (make-leaf 'A 4)
+                  (make-code-tree
+                   (make-leaf 'B 2)
+                   (make-code-tree (make-leaf 'D 1)
+                                   (make-leaf 'C 1)))))
+
+(define sample-message '(A D A B B C A))
+
+(encode sample-message sample-tree)
+
